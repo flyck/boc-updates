5f6faaca4b0a606e39af5e70d11917fa93cfeb91
Felix Brilej <felix.brilej@taxdoo.com>
2023-10-22 11:55:30 +0200

    first iteration toggle

---
+++ src/darkMode.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- Your component content -->
+    Hello i am the dark mode component
+    <DarkModeToggle />
+  </div>
+</template>
+
+<script>
+<!--import * as DarkModeToggle from "dark-mode-toggle"-->
+import * as DarkModeToggle from 'https://googlechromelabs.github.io/dark-mode-toggle/src/dark-mode-toggle.mjs';
+
+const toggle = document.querySelector('dark-mode-toggle');
+const body = document.body;
+
+console.log(toggle)
+// Set or remove the `dark` class the first time.
+toggle.mode === 'dark'
+  ? body.classList.add('dark')
+  : body.classList.remove('dark');
+
+// Listen for toggle changes (which includes `prefers-color-scheme` changes)
+// and toggle the `dark` class accordingly.
+toggle.addEventListener('colorschemechange', () => {
+  body.classList.toggle('dark', toggle.mode === 'dark');
+});
+
+
+</script>
